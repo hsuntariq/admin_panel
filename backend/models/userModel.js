30fa1a682bc80295da74662327da75cfb60e855e
hsuntarq <hsuntariq@gmail.com>
2023-06-07 00:29:22 +0500

    models

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter the name field'],
+        minlength: [3, 'Name should be at least 3 characters long']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter the email field'],
+        unique: true 
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter the password field'],
+        minlength: [6, 'Password should be at least 6 characters long']
+    },
+    role: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
